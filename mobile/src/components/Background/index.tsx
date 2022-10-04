0923674a62153ba7fb76fa5a176f2c811fa01180
victorrcamargo <victorrcamargo@gmail.com>
2022-10-04 11:11:10 -0300

    adding initial files

---
+++ mobile/src/components/Background/index.tsx
@@ -0,0 +1,22 @@
+import { ImageBackground } from "react-native";
+
+import { styles } from "./styles";
+import backgroundImage from "../../assets/background-galaxy.png";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export function Background({ children }: Props) {
+  return (
+    <ImageBackground
+      source={backgroundImage}
+      defaultSource={backgroundImage} //defaultsource vai memorizar a imagem padrão e acelear o carregamento
+      style={styles.container}
+    >
+      {children}
+    </ImageBackground>
+  );
+}
+
+// 1:28h time aula
